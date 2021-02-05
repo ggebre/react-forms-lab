e95a078fceb47810ff30a732413b39d27484d095
Getu Gebre <getu.gebre.gg@gmail.com>
2021-02-04 22:04:02 -0600

    commit tree e176f9f1c4666736369635ca6b37b62830fcb873

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,59 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+  handleUserNameChange = (event) => {
+    let entry = event.target.value 
+    
+    this.setState( {
+        username: entry
+    }
+    )
+  } 
+  handlePasswordChange = (event) => {
+    
+    let entry = event.target.value 
+    this.setState({
+        password: entry
+      }
+    )
+  }
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const username = this.state.username
+    const password = this.state.password
+    if (!!username && !!password){
+      this.props.handleLogin({username,password })
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input 
+                onChange={this.handleUserNameChange} 
+                id="username" 
+                name="username" 
+                value={this.state.username}
+                type="text" />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input 
+                onChange={this.handlePasswordChange} 
+                id="password" 
+                name="password" 
+                value={this.state.password}
+                type="password" />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,42 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      tweet: '',
+      remainingCharCount: this.props.maxChars
+    };
+  }
+  handleOnChange = (event) => {
+    
+    let charEntered = event.target.value
+    this.setState( {
+          tweet: charEntered,
+      }  
+    )
+  }
+  handleOnKeyDown = (event) => {
+    let backspaceCode = event.keyCode
+    this.setState(previousState => {
+      return {
+        remainingCharCount:  backspaceCode == 8 ? previousState.remainingCharCount + 1 : previousState.remainingCharCount - 1
+      }  
+    }
+    )
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input onChange={this.handleOnChange} 
+                onKeyDown={this.handleOnKeyDown} 
+                type="text" name="message" id="message" />
+        <strong>Remaining Characters: {this.state.remainingCharCount}</strong>
+        
+        <p><strong>Characters Entered:</strong> {this.state.tweet}</p>
       </div>
     );
   }
